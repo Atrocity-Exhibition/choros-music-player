52646d1e123011b15a98841e421be7b75edf6ae7
Atrocity-Exhibition <buisnessforahul@gmail.com>
2026-05-27 19:00:26 +0530

    Initial Tauri setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="h-screen bg-zinc-950 text-white flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">
+        Offline Music Player
+      </h1>
+
+      <button className="px-6 py-3 rounded-xl bg-green-500 hover:bg-green-400 transition">
+        Play Music
+      </button>
+    </div>
+  );
+}
+
+export default App;
